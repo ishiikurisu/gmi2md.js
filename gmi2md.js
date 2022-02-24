4377874e894169d451d3656d6ae701e8e0c5cdba
Cris Jr <crisjr@pm.me>
2022-02-24 16:21:53 -0300

    Sketch first solution

--- gmi2md.js
+++ gmi2md.js
@@ -1,5 +1,97 @@
+function _linekind(line) {
+	var kind = "paragraph";
+
+	// code delimiter
+	if (line === "```") {
+		kind = "code";
+	}
+	// link
+	else if (line.substring(0, 2) === "=>") {
+		kind = "link";
+	}
+	// blockquote
+	else if (line[0] === ">") {
+		kind = "quote";
+	}
+	// list
+	else if (line[0] === "*") {
+		kind = "list";
+	}
+	// header
+	else if (line[0] === "#") {
+		kind = "header";
+	}
+	
+	return kind;
+}
+
+function _line2link(line) {
+	const fields = line.split(" ");
+	const href = fields[1];
+	const limit = fields.length;
+	var text = fields[2];
+
+	for (var i = 3; i < limit; i++) {
+		var field = fields[i];
+		text += " " + field;
+	}
+
+	return `[${text}](${href})`;
+}
+
+function _addline(outlet, line, lineKind) {
+	switch (lineKind) {
+		case "link":
+			outlet += _line2link(line) + "\n\n";
+			break;
+		case "quote":
+		case "list":
+		case "code":
+			outlet += line + "\n";
+			break;
+		default:
+			outlet += line + "\n\n";
+	}
+
+	return outlet;
+}
+
 function gmi2md(inlet) {
-    var outlet = "";
-    // TODO implement me!
-    return outlet;
+	var outlet = "";
+	const lines = inlet.split("\n");
+	const limit = lines.length;
+	var insideBlock = false;
+	var insideCodeBlock = false;
+
+	for (var i = 0; i < limit; i++) {
+		var line = lines[i];
+		var lineKind = _linekind(line);
+		
+		if (insideCodeBlock) {
+			outlet += line + "\n";
+			if (lineKind === "code") {
+				insideCodeBlock = false;
+				outlet += "\n";
+			}
+		}
+		else if (insideBlock && (lineKind === "quote" || lineKind === "list")) {
+			outlet += line + "\n";
+		}
+		else {
+			if (lineKind === "quote" || lineKind === "list") {
+				insideBlock = true;
+			}
+			else if (lineKind === "code") {
+				insideCodeBlock = true;
+			}
+			else if (insideBlock) {
+				insideBlock = false;
+				outlet += "\n";
+			}
+
+			outlet = _addline(outlet, line, lineKind);
+		}
+	}
+
+	return outlet;
 }
--- test.js
+++ test.js
@@ -1,13 +1,81 @@
-const example1 = `# 
-`;
+const gemini_text_1 = `# First header
+This is an example of gemini text.
+In gemini, every line is a different paragraph.
+* This is a list in Gemini
+* Lists are also separated from the rest of the text
+Which means this paragraph is separated from the list
+> This is a blockquote
+Yet another paragraph.
+## Differences to HTTP
+### Links
+In Gemini, just like Gopher, links are in separated lines
+=> gemini://geminispace.info/ Gemini Space Info Search Engine
+Some links in some browsers may display images, but not this one.
+### Code blocks
+Not really a difference, but they also appear in Gemini:
+\`\`\`
+#include <stdio.h>
 
-const expected1 = `
+int main() {
+	printf("Hello world!\\n");
+	return 0;
+}
+\`\`\`
+And I think that's all!`;
+
+const markdown_text_1 = `# First header
+
+This is an example of gemini text.
+
+In gemini, every line is a different paragraph.
+
+* This is a list in Gemini
+* Lists are also separated from the rest of the text
+
+Which means this paragraph is separated from the list
+
+> This is a blockquote
+
+Yet another paragraph.
+
+## Differences to HTTP
+
+### Links
+
+In Gemini, just like Gopher, links are in separated lines
+
+[Gemini Space Info Search Engine](gemini://geminispace.info/)
+
+Some links in some browsers may display images, but not this one.
+
+### Code blocks
+
+Not really a difference, but they also appear in Gemini:
+
+\`\`\`
+#include <stdio.h>
+
+int main() {
+	printf("Hello world!\\n");
+	return 0;
+}
+\`\`\`
+
+And I think that's all!
+
 `;
 
 describe("Convert from Gemtext to Markdown", function() {
-    it("can convert from gemtext to markdown", function(done) {
-        var result = gmi2md(example1);
-        chai.assert.ok(result === expected1);
-        done();
-    });
+	it("can convert from gemini links to markdown links", function() {
+		var link = "=> gemini://rawtext.club/~sloum/spacewalk.gmi Link to Space Walk";
+		var expected = "[Link to Space Walk](gemini://rawtext.club/~sloum/spacewalk.gmi)";
+		var result = _line2link(link);
+		chai.assert.ok(result === expected);
+	});
+
+	it("can convert from gemtext to markdown", function() {
+		var result = gmi2md(gemini_text_1);
+
+		chai.assert.ok(result === markdown_text_1);
+	});
 });
