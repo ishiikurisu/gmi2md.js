f1b3561c6887a6b85bdac6ff0f7f2fabdcc0711b
Cris Jr <crisjr@pm.me>
2022-02-25 17:27:45 -0300

    Use regex to parse link

--- test.js
+++ test.js
@@ -67,10 +67,24 @@ And I think that's all!
 
 describe("Convert from Gemtext to Markdown", function() {
 	it("can convert from gemini links to markdown links", function() {
-		var link = "=> gemini://rawtext.club/~sloum/spacewalk.gmi Link to Space Walk";
-		var expected = "[Link to Space Walk](gemini://rawtext.club/~sloum/spacewalk.gmi)";
-		var result = _line2link(link);
-		chai.assert.ok(result === expected);
+		var links = [
+			"=> gemini://rawtext.club/~sloum/spacewalk.gmi Link to Space Walk",
+			"=> ./gemlog/index.gemini Index Page",
+			"=> gemini://gemini.circumlunar.space      Circumlunar Space",
+			"=> https://www.crisjr.eng.br Respect    my     space"
+		];
+		var expected_results = [
+			"[Link to Space Walk](gemini://rawtext.club/~sloum/spacewalk.gmi)",
+			"[Index Page](./gemlog/index.gemini)",
+			"[Circumlunar Space](gemini://gemini.circumlunar.space)",
+			"[Respect    my     space](https://www.crisjr.eng.br)"
+		];
+		for (var i = 0; i < links.length; i++) {
+			var link = links[i];
+			var expected = expected_results[i];
+			var result = _line2link(link);
+			chai.assert.ok(result === expected);
+		}
 	});
 
 	it("can convert from gemtext to markdown", function() {
